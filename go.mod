29934151f2b4c7426a819fd97fe43a8944c8df84
filipearray <codeiesquecepo@gmail.com>
2024-04-29 12:50:45 -0300

    200

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/filipearray/requests
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+import "net/http"
+
+func main(){
+	url := "https://api.ipify.org/?format=json"
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		fmt.Println("Something went wrong:", err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Something went wrong: ", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusFound {
+        distopiaHeader := resp.Header.Get("Distopia")
+        fmt.Println("Valor do header Distopia:", distopiaHeader)
+    } else {
+		fmt.Println("Status Code:", resp.StatusCode, "is not a redirect.")
+	}
+}
